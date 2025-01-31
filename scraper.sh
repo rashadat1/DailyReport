76fe0ea33b6b75ea087cb26ced1c7d3e6e747982
rashadat1 <rashadat@yahoo.com>
2025-01-31 10:37:12 -0500

    begin weather json parsing

--- scraper.sh
+++ scraper.sh
@@ -3,7 +3,13 @@ if [ -z "$1" ]; then
     echo "Usage: $0 <location>"
     exit 1
 fi
-
+# define color codes
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+CYAN="\033[36m"
+RESET="\033[0m"
 location="$1"
 apiKey="$WEATHER_API_KEY"
 unit="$f"
@@ -13,7 +19,16 @@ get_weather_data() {
         --url "https://yahoo-weather5.p.rapidapi.com/weather?location=$location&format=json&u=$unit" \
         --header "x-rapidapi-host: yahoo-weather5.p.rapidapi.com" \
         --header "x-rapidapi-key: $apiKey")
-    echo $response_json | jq
+    echo $response_json
 }
-echo "Getting weather data for $location"
-get_weather_data
+echo -e "${GREEN}Fetching weather data for $location...${RESET}"
+
+parse_weather_data() {
+    weather_data=$(get_weather_data)
+    current_observation=$(echo "$weather_data" | jq '.current_observation')
+    location_data=$(echo "$weather_data" | jq '.location')
+    timestamp=$(echo "$current_observation" | jq '.pubDate')
+    
+    echo "$(date -r $timestamp +%m/%d\ %H:%M)"
+}
+parse_weather_data
